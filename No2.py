8ce2ff2551c0c3af28f60605d7ae5b00576cbbd9
Fanul1 <102352999+Fanul1@users.noreply.github.com>
2022-05-19 21:16:39 +0700

    Update No2.py

--- No2.py
+++ No2.py
@@ -1,11 +1,14 @@
 #Masukkan kalimat
 kalimat = input("Input kalimat: ")
-new_kalimat = [] 
+#Kalimat awal
+new_kalimat = []
+#hufur vokal
 vokal = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', "o", "u"]
 for kata in kalimat:
     new_kata = kata
     for huruf in kata:
         if huruf in vokal:
+            #mengganti huruf vokal dengan bahasa G
             new_kata = new_kata.replace('a', 'aga')
             new_kata = new_kata.replace('A', 'Aga')         
             new_kata = new_kata.replace('e', 'ege')
