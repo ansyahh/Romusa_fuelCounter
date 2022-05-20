813ebcdc9504aaef4df8c4d61e54312c78d56e97
hilalhabib <102476649+hilalhabib@users.noreply.github.com>
2022-05-20 16:54:17 +0700

    Update soal4.py

--- soal4.py
+++ soal4.py
@@ -1,4 +1,7 @@
-daftarAngka = [11,4,5,2,5,6,93,29]
+daftarAngka = []
+for i in range (8):
+    x = int(input("Masukkan Angka: "))
+    daftarAngka.append(x)
 print('Daftar Angka Sebelum di Sorting: ', daftarAngka)
 list.sort(daftarAngka)
 print('Daftar Angka Sesudah di Sorting: ', daftarAngka)
